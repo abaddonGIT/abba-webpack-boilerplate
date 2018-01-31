7f0b0e2ed795aabbb2f1420908a0709aa574c630
abaddonGIT <abaddongit@gmail.com>
2018-01-31 13:01:14 +0300

    add atomic styles, fix font gulp task

--- gulp/tasks/fonts.js
+++ gulp/tasks/fonts.js
@@ -1,20 +1,20 @@
 /**
  * Сканирует папку со шрифтами и формирует less файл для них
  */
-var gulp = require("gulp"),
-    fs = require("fs"),
-    file = require('gulp-file'),
-    paths = require('../../config/environments.config').fonts;
+const gulp = require("gulp"),
+  fs = require("fs"),
+  file = require('gulp-file'),
+  paths = require('../../config/environments.config').fonts;
 
 
 gulp.task('fonts', function () {
-    var code = "";
+  let code = "";
 
-    function getFolders() {
-        return fs.readdirSync(paths.src)
-            .filter(function (file) {
-                if (file.split('.').length < 2) {
-                    code += `@font-face {
+  function getFolders() {
+    return fs.readdirSync(paths.src)
+      .filter(function (file) {
+        if (file.split('.').length < 2) {
+          code += `@font-face {
                         font-family: "${file}";
                         src: url("./fonts/${file}/${file}.eot");
                         src: url("./fonts/${file}/${file}.eot?#iefix")format("embedded-opentype"),
@@ -22,12 +22,16 @@ gulp.task('fonts', function () {
                         url("./fonts/${file}/${file}.ttf") format("truetype");
                         font-style: normal;
                         font-weight: normal;
-                    }`;
-                }
-            });
-    }
+                    } 
+                    .${file} {
+                      font-family: "${file}", sans-serif;
+                    }
+                    `;
+        }
+      });
+  }
 
-    getFolders();
+  getFolders();
 
-    return file('fonts.less', code).pipe(gulp.dest(paths.dist));
+  return file('fonts.less', code).pipe(gulp.dest(paths.dist));
 });
