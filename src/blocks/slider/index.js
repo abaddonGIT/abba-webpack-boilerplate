0f8fbc079662073cbec8c6aaff9b827d16a3cc79
abaddonGIT <abaddongit@gmail.com>
2020-01-02 07:09:50 +0300

    change main slider block

--- src/blocks/slider/index.js
+++ src/blocks/slider/index.js
@@ -1,25 +1,21 @@
-/**
- * Created by Abaddon on 26.02.2017.
- */
 import '../../js/vendors/flex/flexslider.css';
 
 require('../../js/vendors/flex/jquery.flexslider-min');
 
-class MainSlider {
-  constructor(slider) {
-    this.slider = slider;
-  }
-
-  init() {
-    if (this.slider) {
-      const speed = +this.slider.getAttribute('data-speed');
-      $(this.slider).flexslider({
-        touch: true,
-        slideshowSpeed: Number.isNaN(speed) ? 5000 : speed,
-        animationSpeed: 2000,
+const mainSlider = (domSlider, options = {
+  touch: true,
+  animationSpeed: 2000,
+}) => {
+  if (domSlider) {
+    const speed = +domSlider.getAttribute('data-speed');
+    $(domSlider)
+      .flexslider({
+        ...options,
+        ...{
+          slideshowSpeed: Number.isNaN(speed) ? 5000 : speed,
+        },
       });
-    }
   }
-}
+};
 
-export default new MainSlider();
+export default mainSlider;
